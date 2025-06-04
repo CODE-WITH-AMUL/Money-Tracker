9abd9df38bbf54d204c24a05c217e16e25a52067
AMUL SHARMA <140779732+CODE-WITH-AMUL@users.noreply.github.com>
2025-06-05 03:53:44 +0545

    Update settings.py

--- MoneyTracker/settings.py
+++ MoneyTracker/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=70q$n_92-ea7uqnq7divd@0rut92vgcykgtylefl_a@&34sfh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
